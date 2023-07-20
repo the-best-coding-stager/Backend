c0b47019455e01ee891b08d9ae83fb236353ee61
Joo Hyun Lee <michelle8085@gmail.com>
2023-07-20 16:50:11 +0900

    Slang 백업

--- Slang/src/main/java/com/bestcodingstager/slang/service/LikeService.java
+++
@@ -1,7 +0,0 @@
-package com.bestcodingstager.slang.service;
-
-import com.bestcodingstager.slang.dto.*;
-
-public interface LikeService {
-
-}
--- Slang/src/main/java/com/bestcodingstager/slang/service/UserService.java
+++
@@ -1,9 +0,0 @@
-package com.bestcodingstager.slang.service;
-
-import java.util.List;
-
-import com.bestcodingstager.slang.dto.User;
-
-public interface UserService {
-	List<User> getAllUsers();
-}
